415a17e82d67038364e4e4cbb2cab7cb816928a3
Parth <developmentprogramming154@gmail.com>
2020-04-27 23:19:56 +0200

    Update

--- controllers/gallery.js
+++ controllers/gallery.js
@@ -1,7 +1,7 @@
 const db = require('knex')({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   }
 })
