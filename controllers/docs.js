b843141fb73563157780feb7bd05ee1ea358825c
Parth <developmentprogramming154@gmail.com>
2020-05-12 17:55:32 +0200

    updated usage endpoint

--- controllers/docs.js
+++ controllers/docs.js
@@ -150,7 +150,7 @@ const usage = async (target, db, res) => {
   })
 
   var id = setInterval(() => {
-    if(data.length === finalContent.length)
+    if(data.length === finalContent.length && finalContent.length !== 0)
       res.status(200).json(finalContent)
 
       clearInterval(id)
